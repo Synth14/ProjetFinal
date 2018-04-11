186719d9f4c4e0e9c2285ccd6b49d9b7dbfd8556
Syn Scorps <iasyasdnn@live.fr>
2018-04-11 23:01:48 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetFinal_UIL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetFinal_UIL.Startup))]
+namespace ProjetFinal_UIL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
