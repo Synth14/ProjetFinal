4dc1d8bfdccbf93c9f5d5594162fd5b5f2ca3cd5
Syn Scorps <iasyasdnn@live.fr>
2018-04-12 00:52:56 +0200

    Rajout liste client+ tout.

---
+++ ProjetFinal_UIL/Controllers/ClientsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetFinal_DAL;
+
+namespace ProjetFinal_UIL.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private FinalDBContext db = new FinalDBContext();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+
+     
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
