b9bb101879c9961ad7c97b7bfcca119f179c61f1
sylvie chassery <sylviechassery@gmail.com>
2018-04-15 15:31:11 +0200

    Design

--- ProjetFinal_DAL/DBModel.Designer.cs
+++ ProjetFinal_DAL/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Adminl\Source\Repos\ProjetFinal\ProjetFinal_DAL\DBModel.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\Adminl\source\repos\ProjetFinal\ProjetFinal_DAL\DBModel.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
